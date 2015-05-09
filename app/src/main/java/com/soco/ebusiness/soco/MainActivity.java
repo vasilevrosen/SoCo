8fe12781f87e724ab16c16a81426465eaf549b5c
Rosen Vasilev <varo1012@hs-karlsruhe.de>
2015-05-09 20:43:54 +0200

    Side Menu

--- app/src/main/java/com/soco/ebusiness/soco/MainActivity.java
+++ app/src/main/java/com/soco/ebusiness/soco/MainActivity.java
@@ -103,7 +103,7 @@ public class MainActivity extends ActionBarActivity
         if (id == R.id.action_settings) {
             return true;
         }
-
+        
         return super.onOptionsItemSelected(item);
     }
 
