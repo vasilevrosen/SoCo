cffe9a6f99a3706c16062b80a132b2a0991da7ef
Benjamin Teetsch <benjamin.teetsch@yahoo.de>
2015-05-16 04:52:48 +0200

    Grundgerüst von KocheventAnbietem fertig inkl. Datum und Uhrzeit-Auswahl

---
+++ app/src/main/java/com/soco/ebusiness/soco/FragmentTestFragment.java
@@ -0,0 +1,23 @@
+package com.soco.ebusiness.soco;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class FragmentTestFragment extends Fragment {
+
+    public FragmentTestFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fragment_test, container, false);
+    }
+}
--- app/src/main/java/com/soco/ebusiness/soco/KocheventAnbietenActivity.java
+++ app/src/main/java/com/soco/ebusiness/soco/KocheventAnbietenActivity.java
@@ -1,19 +1,38 @@
 package com.soco.ebusiness.soco;
 
+import android.app.DialogFragment;
+import android.app.FragmentManager;
 import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
-public class KocheventAnbietenActivity extends ActionBarActivity {
+public class KocheventAnbietenActivity extends FragmentActivity {
+
+
+    private static EditText gesetztesDatum;
+    private static EditText gesetzteZeit;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kochevent_anbieten);
+        gesetztesDatum = (EditText) findViewById(R.id.editTextEventDatum);
+        gesetzteZeit = (EditText) findViewById(R.id.editTextEventUhrzeit);
+
     }
 
     public void navigateToRezeptsuche(View view){
@@ -22,6 +41,37 @@ public class KocheventAnbietenActivity extends ActionBarActivity {
         startActivity(intent);
     }
 
+    public void editEventDatum(View view){
+
+        DialogFragment dateFragment = new DatePickerFragment();
+        dateFragment.show(getFragmentManager(), "datePicker");
+    }
+
+
+    public void editEventUhrzeit(View view){
+
+        DialogFragment timeFragment = new TimePickerFragment();
+        timeFragment.show(getFragmentManager(), "timePicker");
+    }
+
+    public static void setDate(int tag, int monat, int jahr){
+
+
+        String neuesDatum = String.format("%02d", tag) + "." + String.format("%02d", monat) + "." + String.format("%02d", jahr);
+
+        gesetztesDatum.setText(neuesDatum);
+
+
+    }
+
+
+    public static void setTime(int stunden, int minuten){
+
+
+
+        String neueZeit = String.format("%02d", stunden) + ":" + String.format("%02d", minuten);
+        gesetzteZeit.setText(neueZeit);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
